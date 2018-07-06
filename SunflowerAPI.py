7b3204bd020a036a68e4ad9622f42b80f0feb9f5
aalhaji <38117439+aalhaji@users.noreply.github.com>
2018-07-06 11:39:20 +0200

    updated API integration
    deleted swap files and updated API integration

--- SunflowerAPI.py
+++ SunflowerAPI.py
@@ -15,7 +15,7 @@ import json
 import uuid
 import socket
 
-api_url_base = 'https://api-test.countr.rest/v2/' #'http://192.168.43.154:3030/v2/'  
+api_url_base = 'https://api-test.countr.rest/v2/' #'http://192.168.43.154:3030/v2/'
 
 username = 'demo@countrhq.com'
 password = 'Demo12345'
@@ -30,147 +30,138 @@ s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 s.connect(("8.8.8.8", 80))
 ip_address = s.getsockname()[0]
 
-
-###### FUNCTIONS #######
-
-############ ACCESS LIBRARY  ################
+################### ACCESS  ################
 
 class Access:
 
 ############ FIRST TIME LOGIN ##############
-    
+
     def authenticate(username, password, client_id, client_secret, grant_type):
-        
-        auth={"username":username,"password":password, "client_id":client_id, "client_secret":client_secret, "grant_type":grant_type}
-        
-        resp = requests.post(api_url_base+'oauth/token', data=auth)
-                
+
+        body ={"username":username,"password":password, "client_id":client_id, "client_secret":client_secret, "grant_type":grant_type}
+        resp = requests.post(api_url_base+'oauth/token', data=body)
+
         if resp.status_code != 201:
             print("Authorization error. Error code: ", resp.status_code)
 
-            
         Access.access_token = resp.json()["access_token"]
         Access.refresh_token = resp.json()["refresh_token"]
         Access.bearer = 'Bearer '+str(Access.access_token)
- 
+
         print(resp.json())
-        
+
         print('Logged in. Access Token: {}'.format(Access.access_token))
-    
+
 ############ REFRESH TOKEN ##################
-        
+
     def refresh(grant_type, client_id, client_secret, refresh_token):
-        
-        refr={"grant_type":grant_type, "client_id":client_id, "client_secret":client_secret, "refresh_token":refresh_token}
-        
-        rresp = requests.post(api_url_base+'oauth/refresh', data=refr)
-        
-        if rresp.status_code != 200:
-            print("Refresh token error. Error code: ", rresp.status_code)
-            
-        Access.access_token = rresp.json()["access_token"]
-        Access.refresh_token = rresp.json()["refresh_token"]
+
+        body ={"grant_type":grant_type, "client_id":client_id, "client_secret":client_secret, "refresh_token":refresh_token}
+        resp = requests.post(api_url_base+'oauth/refresh', data=body)
+
+        if resp.status_code != 200:
+            print("Refresh token error. Error code: ", resp.status_code)
+
+        Access.access_token = resp.json()["access_token"]
+        Access.refresh_token = resp.json()["refresh_token"]
         Access.bearer = 'Bearer '+str(Access.access_token)
-        
-        print(rresp.json())
+
+        print(resp.json())
 
         print('Token refreshed. Access Token: {}'.format(Access.access_token))
 
         return Access.access_token, Access.refresh_token
 
 ########### GET USER INFORMATION ################
-        
+
     def getuserinfo(access_token):
 
         while True:
+            body = {"Authorization":Access.bearer}
+            resp = requests.get(api_url_base+'me', json=body, headers=body)
 
-            userinf = {"Authorization":Access.bearer}
-            userresp = requests.get(api_url_base+'me', json=userinf, headers=userinf)
-
-            if userresp.status_code == 200:
-
-                print(userresp.json())
+            if resp.status_code == 200:
+                print(resp.json())
                 print('==================================')
                 break
-            
-            else:
 
-                misc.Exception_Handler(userresp.status_code)
-                
+            else:
+                misc.Exception_Handler(resp.status_code)
+
 
 ################ GET STORES #######################
 
     def getStores(access_token):
 
-        storebody = {"Authorization":Access.bearer}
-        storeresp = requests.get(api_url_base+'stores', json=storebody, headers=storebody)
+        while True:
 
-        print(storeresp.json())
-        print('=========================')
+            body = {"Authorization":Access.bearer}
+            resp = requests.get(api_url_base+'stores', json=body, headers=body)
 
+            if resp.status_code == 200:
+                print(resp.json())
+                print('==================================')
+                break
+
+            else:
+                misc.Exception_Handler(resp.status_code)
 
 ############# END CLASS 'ACCESS' ##################
-
 
 ##################### DEVICES #####################
 
 class Devices:
-    
+
 ############## CREATE  DEVICE #####################
-    
+
     def CreateDevice(name, uuid, store):
-        
-        device = {"name":name, "uuid":uuid, "store":store}
-        
-        devresp = requests.post(api_url_base+'devices', data=device, headers={"Authorization":Access.bearer, "Content-Type":'application/x-www-form-urlencoded'})
-        
-        if devresp.status_code != 200:
-           # if devresp.status_code == 401:
-             #   print("Need to refresh token.")
-               # Access.refresh("password", client_id, client_secret, Access.refresh_token)
-            #    print("Token refreshed. Please try again.")
-          #  else:
-            print("Create device error. Error code: ", devresp.status_code)
-           
-        print(devresp.json())
+
+        while True:
+            body = {"name":name, "uuid":uuid, "store":store}
+            resp = requests.post(api_url_base+'devices', data=body, headers={"Authorization":Access.bearer, "Content-Type":'application/x-www-form-urlencoded'})
+
+            if resp.status_code == 200:
+                print(resp.json())
+                print('==================================')
+                break
+
+            else:
+                misc.Exception_Handler(resp.status_code)
+
+
 
 ############# GET SPECIFIC DEVICE ################
 
     def GetDevice(name, uuid):
-        
-        device = {"name":name, "uuid":uuid}, 
 
-        devresp = requests.get(api_url_base+'devices/'+uuid, data=device, headers={"Authorization":Access.bearer, "Content-Type":'application/x-www-form-urlencoded'})
+        while True:
+            body = {"name":name, "uuid":uuid},
+            resp = requests.get(api_url_base+'devices/'+uuid, data=body, headers={"Authorization":Access.bearer, "Content-Type":'application/x-www-form-urlencoded'})
 
-        if devresp.status_code != 200:
-            #if devresp.status_code == 401:
-              #  print("Need to refresh token."
-              #  Access.refresh("password", client_id, client_secret, Access.refresh_token)
-             ##   print("Token refreshed. Please try again.")
-         #   else:
-            print("Get device error. Error code: ", devresp.status_code)
-            
+            if resp.status_code == 200:
+                print(resp.json())
+                print('==================================')
+                break
 
-        print(devresp.json())
+            else:
+                misc.Exception_Handler(resp.status_code)
 
 ############# SEND IP ADDRESS #####################
 
-		
+
     def PatchIP(name, uuid,  ip_address):
-	
-	    body = {"name":name, "uuid":uuid, "info.ip_address":ip_address}
-		
-	    patchresp = requests.patch(api_url_base+'devices/'+uuid, data=body, headers={"Authorization":Access.bearer})
-		
-	    if patchresp.status_code != 200:
-               # if patchresp.status_code == 401:
-                  #  print("Need to refresh token."
-                  #  Access.refresh("password", client_id, client_secret, Access.refresh_token)
-                   # print("Token refreshed. Please try again.")
-               # else:
-                 print("Patch device error. Error code: ", patchresp.status_code)
-			
-	    print(patchresp.json())
+
+        while True:
+            body = {"name":name, "uuid":uuid, "info.ip_address":ip_address}
+            resp = requests.patch(api_url_base+'devices/'+uuid, data=body, headers={"Authorization":Access.bearer})
+
+	    if resp.status_code == 200:
+            print(resp.json())
+            print('==================================')
+            break
+
+        else:
+            misc.Exception_Handler(resp.status_code)
 
 ########### END CLASS 'DEVICES' ####################
 
@@ -178,10 +169,10 @@ class Devices:
 
 class misc:
 
-    def Exception_Handler(resp):
-        
-        print("Error. Status code: ", status_code)
-        
+    def Exception_Handler(status_code):
+
+        print("Exception. Status code: ", status_code)
+
         if status_code == 401:
             print("Need to refresh token.")
             Access.refresh('password', client_id, client_secret, Access.refresh_token)
@@ -189,13 +180,11 @@ class misc:
 
         ## other errors
 
-
-
 ################### END CLASS 'MISC' ###############
 
 
 
-		
+
 #### testing zone ####
 
 Access.authenticate(username, password, client_id, client_secret, grant_type)
