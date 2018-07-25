10f3e093ebad86e624cc622fe762b178ac4b55ce
aalhaji <aalhaji@u.rochester.edu>
2018-07-25 13:20:07 +0200

    changed buttonListener to account for waveform produced by bed rather than a pwm input

--- buttonListener.py
+++ buttonListener.py
@@ -1,32 +1,16 @@
 import automationhat as shield
 import time
-time.sleep(0.1)
 
-wait = 1
-
-# Initialization
-try:
-    shield.relay.one.read()
-except RuntimeError:
-    shield.relay.one.read()
-# End initialization
-
-# The variable "is_pressed" is meant to indicate the last stete of the button
-# call it condittionally in the app, and reset it to 0 once
-# the last has been fulfilled
-
-class Listen():
-
-    def buttonlisten():
-        while True:
-            if shield.input.one.is_on():
-                print("Button pressed.")
-                Listen.is_pressed = 1
-                break
-
+time_now = time.time()
 
 while True:
-    Listen.buttonlisten()
-    time.sleep(wait)
-    Listen.buttonlisten()
-    time.sleep(wait)
+
+    value = shield.analog.one.read()
+
+    if (value > 1):
+
+        if ((time.time()-time_now) > 0.3):
+            print(value)
+            print("button pressed.")
+            
+        time_now = time.time()
