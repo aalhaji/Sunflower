73c5194b772cab10022d214ec1a2197f4adde170
Abdulwahab <abdul@countrhq.com>
2019-06-05 17:09:32 +0200

    updating

--- sfmain.py
+++ sfmain.py
@@ -135,8 +135,14 @@ def bedoff():
 
     elif currentState == 2: # cooldown
 
+<<<<<<< HEAD
         transitions.afterCool()
         currentState = states.checkLocalState()
+=======
+        currentState = 3
+        states.updateLocalState(currentState)
+        states.updateServerState()
+>>>>>>> parent of 0c98166... removing repetition
         str_state = states_dict[currentState]
 
         return "Cooldown was interrupted. Bed is now in state: {}".format(str_state)
