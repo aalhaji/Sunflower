d8ce4b3c07557082f78173c1236e5289628fe03b
Abdulwahab <abdul@countrhq.com>
2019-06-06 15:23:18 +0200

    update to py3

--- internet_on.py
+++ internet_on.py
@@ -1,10 +1,10 @@
 # this one-function module checks for an internet connection
 
-import urllib2
+rom urllib.request import urlopen
 
 def internet_on():
     try:
-        urllib2.urlopen('http://216.58.192.142', timeout=1) # Google
+        urllib2.urlopen('http://1.1.1.1', timeout=1) # generic
         return True
     except urllib2.URLError as err:
         return False
