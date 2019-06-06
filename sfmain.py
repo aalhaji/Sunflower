138ff94de3fb361079b9a472dbbeca0a3d01c79a
Abdulwahab <abdul@countrhq.com>
2019-06-06 12:52:19 +0200

    removed annoying popups

--- sfmain.py
+++ sfmain.py
@@ -86,14 +86,10 @@ states_dict={0:"AVAILABLE(OFF)",
 # START APP
 
 from flask import Flask, render_template, flash, request
-from wtforms import Form, TextField, TextAreaField, validators, StringField, SubmitField
-
 
 # App config.
-DEBUG = True
+#DEBUG = True # debug mode
 app = Flask(__name__)
-app.config.from_object(__name__)
-app.config['SECRET_KEY'] = '7d441f27d441f27567d441f2b6176a'
 
 @app.route('/')
 def home():
